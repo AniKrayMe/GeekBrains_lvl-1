7da146997594c7cd0f11f492bf053c2c0a933868
AniKrayMe <sseriann@mail.ru>
2021-03-11 19:47:18 +0300

    Homework-6

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,19 +1,10 @@
 package com.company;
 
+
 public class Main {
 
     public static void main(String[] args) {
 
-        Cat cat = new Cat("Мурька");
-        cat.run(200);
-        Dog dog = new Dog("Шарик");
-        dog.run(500);
-        dog.swim(10);
-
-        //Проверка максимального значения
-        dog.run(15000);
-        cat.swim(3);
-
-        System.out.println(Dog.quantity());
+        //Я так и не понял где домашка XDXD
     }
 }
